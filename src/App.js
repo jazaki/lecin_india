e8681430e839410f90199c29766b328abc3489e7
= <=>
2018-04-18 10:20:34 +0530

    created the basic top nav and carousal layout

--- src/App.js
+++ src/App.js
@@ -2,18 +2,37 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import 'antd/dist/antd.css';
+import { Layout, Menu, Carousel} from 'antd';
+const { Header, Content, Footer } = Layout;
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <Layout>
+            <Header style={{height: "10em"}}>
+                <Menu
+                    theme="dark"
+                    mode="horizontal"
+                    defaultSelectedKeys={['1']}
+                    style={{ lineHeight: '5em', float: "right", marginTop: "2.5em" }}>
+                    <Menu.Item key="1">About Us</Menu.Item>
+                    <Menu.Item key="2">Our Programs</Menu.Item>
+                    <Menu.Item key="3">Initiatives</Menu.Item>
+                    <Menu.Item key="4">Events</Menu.Item>
+                    <Menu.Item key="5">Feature</Menu.Item>
+                    <Menu.Item key="6">Contact Us</Menu.Item>
+                </Menu>
+            </Header>
+            <Content>
+                <Carousel autoplay>
+                    <div><h3>1</h3></div>
+                    <div><h3>2</h3></div>
+                    <div><h3>3</h3></div>
+                    <div><h3>4</h3></div>
+                </Carousel>
+            </Content>
+        </Layout>
     );
   }
 }
