df6eccbe1e49239d036568c69b42e7623f084fb4
= <=>
2018-04-19 09:53:46 +0530

    created the basic Grid Layout of home page

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 
 import 'antd/dist/antd.css';
-import { Layout, Menu, Carousel} from 'antd';
-const { Header, Content, Footer } = Layout;
+import { Layout, Menu, Carousel, Row, Col } from 'antd';
+const { Header, Content, Footer} = Layout;
 
 class App extends Component {
   render() {
@@ -31,8 +31,108 @@ class App extends Component {
                     <div><h3>3</h3></div>
                     <div><h3>4</h3></div>
                 </Carousel>
+                <Row>
+                    <Col span={4}></Col>
+                    <Col span={16}>
+                        <h1 style={{textAlign: "center"}}>
+                            About Us
+                        </h1>
+                        <p style={{textAlign: "center"}}>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut placerat egestas mauris, ut tincidunt nunc. Sed vel viverra orci. Nulla tincidunt lectus id volutpat euismod. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nunc ac efficitur sapien. Sed luctus porttitor libero, quis convallis lorem hendrerit eget.
+                        </p>
+                    </Col>
+                    <Col span={4}></Col>
+                </Row>
+                <Row>
+                    <Col span={4}></Col>
+                    <Col span={16}>
+                        <h1 style={{textAlign: "center"}}>
+                            Our Programs
+                        </h1>
+                    </Col>
+                    <Col span={4}></Col>
+                </Row>
+                <Row style={{padding: "20px"}}>
+                    {/*try CSS Grid instead*/}
+                    <Col span={6} style={{height: "15em", border: "1px solid black"}}></Col>
+                    <Col span={6} style={{height: "15em", border: "1px solid black"}}></Col>
+                    <Col span={6} style={{height: "15em", border: "1px solid black"}}></Col>
+                    <Col span={6} style={{height: "15em", border: "1px solid black"}}></Col>
+                </Row>
+                <Row>
+                    <Col span={4}></Col>
+                    <Col span={16}>
+                        <h1 style={{textAlign: "center"}}>
+                            Our Initiatives
+                        </h1>
+                    </Col>
+                    <Col span={4}></Col>
+                </Row>
+                <Row>
+                    {/*try CSS Grid instead*/}
+                    <Col span={4}></Col>
+                    <Col span={16} style={{padding: "20px"}}>
+                        <Row>
+                            <Col span={24} style={{height: "10em", border: "1px solid black"}}></Col>
+                        </Row>
+                        <Row>
+                            <Col span={24} style={{height: "10em", border: "1px solid black"}}></Col>
+                        </Row>
+                        <Row>
+                            <Col span={24} style={{height: "10em", border: "1px solid black"}}></Col>
+                        </Row>
+                    </Col>
+                    <Col span={4}></Col>
+                </Row>
+                <Row>
+                    <Col span={4}></Col>
+                    <Col span={16}>
+                        <h1 style={{textAlign: "center"}}>
+                            News Section
+                        </h1>
+                    </Col>
+                    <Col span={4}></Col>
+                </Row>
+                <Row style={{padding:"20px"}}>
+                    {/*try CSS Grid instead*/}
+                    <Col span={2}></Col>
+                    <Col span={20} style={{height: "20em", border: "1px solid black"}}>
+                    </Col>
+                    <Col span={2}></Col>
+                </Row>
             </Content>
-            <Footer>
+            <Footer style={{background: "#cecece", height: "30em", padding: 0}}>
+                <Row style={{padding:"20px", borderBottom: "1px solid black", height: "10em"}}>
+                    <Col span={12} style={{textAlign: "center"}}>
+                        <h2>Address</h2>
+                    </Col>
+                    <Col span={12} style={{textAlign: "center"}}>
+                        <h2>Social Links</h2>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col span={4}></Col>
+                    <Col span={16}>
+                        <h1 style={{textAlign: "center"}}>
+                            FAQ Form
+                        </h1>
+                    </Col>
+                    <Col span={4}></Col>
+                </Row>
+                <Row style={{padding:"20px", borderBottom: "1px solid black", height: "10em"}}>
+                    <Col span={12} style={{textAlign: "center"}}>
+                        <h4>Email Input</h4>
+                    </Col>
+                    <Col span={12} style={{textAlign: "center"}}>
+                        <h4>Query Input</h4>
+                    </Col>
+                </Row>
+                <Row style={{background: "#000", height: "20%", display: "flex"}}>
+                    <Col span={24}
+                         style={{color: "#fff", display: "flex", alignItems: "center", justifyContent: "center"}}>
+                        &copy;&nbsp;LECIN India 2018 All Rights Reserved
+                    </Col>
+                </Row>
             </Footer>
         </Layout>
     );
