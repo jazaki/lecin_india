123020327177ac9daae0b2c7ea68396600a55424
= <=>
2018-04-24 08:17:10 +0530

    added the fullpage.js

--- src/App.js
+++ src/App.js
@@ -1,85 +1,132 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import $ from 'jquery'
+import 'fullpage.js/dist/jquery.fullpage'
+import 'fullpage.js/dist/jquery.fullpage.css'
 
+import './App.css';
 import 'antd/dist/antd.css';
+
 import { Layout, Menu, Carousel, Row, Col } from 'antd';
-import {SectionsContainer, Section} from 'react-fullpage';
 
 const { Header, Footer, Sider, Content } = Layout;
 
-let options = {
-    sectionClassName:     'section',
-    anchors:              ['sectionOne', 'sectionTwo', 'sectionThree', 'sectionFour', 'sectionFive', 'sectionSix', 'sectionSeven'],
-    scrollBar:            false,
-    navigation:           false,
-    verticalAlign:        false,
-    sectionPaddingTop:    '0',
-    sectionPaddingBottom: '0',
-    arrowNavigation:      true
-};
-
 class App extends Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+    componentDidMount(){
+        $('#fullpage').fullpage({
+            //Navigation
+            menu: '#menu',
+            lockAnchors: true,
+            anchors:['', 'about', 'programs', 'initiatives', 'news', 'contact_us'],
+            navigation: false,
+            navigationPosition: 'right',
+            navigationTooltips: ['firstSlide', 'secondSlide'],
+            showActiveTooltip: false,
+            slidesNavigation: false,
+            slidesNavPosition: 'bottom',
+
+            //Scrolling
+            css3: true,
+            scrollingSpeed: 700,
+            autoScrolling: true,
+            fitToSection: true,
+            fitToSectionDelay: 1000,
+            scrollBar: false,
+            easing: 'easeInOutCubic',
+            easingcss3: 'ease',
+            loopBottom: false,
+            loopTop: false,
+            loopHorizontal: true,
+            continuousVertical: false,
+            continuousHorizontal: false,
+            scrollHorizontally: false,
+            interlockedSlides: false,
+            dragAndMove: false,
+            offsetSections: false,
+            resetSliders: false,
+            fadingEffect: false,
+            normalScrollElements: '#element1, .element2',
+            scrollOverflow: false,
+            scrollOverflowReset: false,
+            scrollOverflowOptions: null,
+            touchSensitivity: 15,
+            normalScrollElementTouchThreshold: 5,
+            bigSectionsDestination: null,
+
+            //Accessibility
+            keyboardScrolling: true,
+            animateAnchor: true,
+            recordHistory: true,
+
+            //Design
+            controlArrows: true,
+            verticalCentered: true,
+            sectionsColor : ['#ccc', '#fff', "#000"],
+            paddingTop: '0',
+            paddingBottom: '0',
+            fixedElements: '#header, .footer',
+            responsiveWidth: 0,
+            responsiveHeight: 0,
+            responsiveSlides: false,
+            parallax: false,
+            parallaxOptions: {type: 'reveal', percentage: 62, property: 'translate'},
+
+            //Custom selectors
+            sectionSelector: '.section',
+            slideSelector: '.slide',
+
+            lazyLoading: true,
+
+            //events
+            onLeave: function(index, nextIndex, direction){},
+            afterLoad: function(anchorLink, index){},
+            afterRender: function(){},
+            afterResize: function(){},
+            afterResponsive: function(isResponsive){},
+            afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
+            onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){}
+        });
     }
 
   render() {
     return (
         <Layout>
-            <Header style={{height: "fit-content", zIndex: 1000, position: "fixed", width: "100%", padding: "2em", background: "transparent"}}>
-                <Menu theme="dark"
-                      mode="horizontal"
-                      style={{ lineHeight: '5em', float: "right", background: "transparent", border: "none"}}>
-                    <Menu.Item key="2">
-                        <a href="#sectionTwo">Section Two</a>
-                    </Menu.Item>
-                    <Menu.Item key="3">
-                        <a href="#sectionThree">Section Three</a>
-                    </Menu.Item>
-                    <Menu.Item key="4">
-                        <a href="#sectionFour">Section Four</a>
-                    </Menu.Item>
-                    <Menu.Item key="5">
-                        <a href="#sectionFive">Section Five</a>
-                    </Menu.Item>
-                    <Menu.Item key="6">
-                        <a href="#sectionSix">Section Six</a>
-                    </Menu.Item>
-                    <Menu.Item key="7">
-                        <a href="#sectionSeven">Section Seven</a>
-                    </Menu.Item>
+            <Header className="nav-bar">
+                <div className="logo" />
+                <Menu
+                    theme="dark"
+                    mode="horizontal"
+                    defaultSelectedKeys={['2']}
+                    style={{ lineHeight: '64px' }}>
+                    <Menu.Item key="1">nav 1</Menu.Item>
+                    <Menu.Item key="2">nav 2</Menu.Item>
+                    <Menu.Item key="3">nav 3</Menu.Item>
                 </Menu>
             </Header>
-            <SectionsContainer {...options}>
-                <Section>
-                    {/*<div style={{background: "#2ce6cc", height: "100%"}}></div>*/}
-                    <Carousel autoplay mobileFirst="true">
-                        <div><h3>1</h3></div>
-                        <div><h3>2</h3></div>
-                        <div><h3>3</h3></div>
-                        <div><h3>4</h3></div>
-                    </Carousel>
-                </Section>
-                <Section>
-                    <div style={{background: "#b1cac6", height: "100%"}}></div>
-                </Section>
-                <Section>
-                    <div style={{background: "#e66f6a", height: "100%"}}></div>
-                </Section>
-                <Section>
-                    <div style={{background: "#17ade6", height: "100%"}}></div>
-                </Section>
-                <Section>
-                    <div style={{background: "#dee633", height: "100%"}}></div>
-                </Section>
-                <Section>
-                    <div style={{background: "#a666e6", height: "100%"}}></div>
-                </Section>
-                <Section>
-                    <div style={{background: "#e6235d", height: "100%"}}></div>
-                </Section>
-            </SectionsContainer>
+            <Content>
+                <div id="fullpage">
+                    <div className="section">
+                        <Carousel autoplay>
+                            <div><h3>1</h3></div>
+                            <div><h3>2</h3></div>
+                            <div><h3>3</h3></div>
+                            <div><h3>4</h3></div>
+                        </Carousel>
+                    </div>
+                    <div className="section">
+
+                    </div>
+                    <div className="section">
+
+                    </div>
+                    <div className="section">
+
+                    </div>
+                </div>
+            </Content>
         </Layout>
     );
   }
